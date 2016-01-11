e059a75b242c9ffaedf71b59b99fef8b49fdc1c6
Emily Nelson <emilymay42@gmail.com>
2016-01-11 22:50:37 +0000

    Seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Place.create(name: 'Pizza Nomster', address: '2 Main Street, Cambridge, MA 02139', description: 'Best pizza in Cambridge. Their crust is to die for!')
+Place.create(name: 'Area 4', address: '112 Main Street, Cambridge, MA 02139', description: 'One of the best pizza places. Their mushroom pizza is unsurpassed!')
