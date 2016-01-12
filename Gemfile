a3238f11001be08d52fea18e4ce3d6b9e990ccfe
Emily Nelson <emilymay42@gmail.com>
2016-01-12 22:04:03 +0000

    Added pagination

--- Gemfile
+++ Gemfile
@@ -37,6 +37,9 @@ gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'sprockets', '2.11.0'
 gem 'sass-rails'
 
+# Add pagination 
+gem 'will_paginate'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,10 @@
 
 Place.create(name: 'Pizza Nomster', address: '2 Main Street, Cambridge, MA 02139', description: 'Best pizza in Cambridge. Their crust is to die for!')
 Place.create(name: 'Area 4', address: '112 Main Street, Cambridge, MA 02139', description: 'One of the best pizza places. Their mushroom pizza is unsurpassed!')
+Place.create(name: 'Los Reyes', address: '3 Town Square Plaza', description: 'Cheap and delicious Mexican')
+Place.create(name: 'Pure Grain Bakery', address: '10 Main Street', description: 'Fresh baguettes baked daily, and an amazing dessert counter')
+Place.create(name: 'Merchant and Main', address: '45 Main Street', description: 'A beloved fixture of old downtown Vacaville')
+Place.create(name: 'Strings', address: '756 Nut Tree Road, Vacaville, CA 95687', description: 'Best Italian food in the city')
+Place.create(name: 'Clay Oven', address: '23 Orange Drive, Vacaville, CA 95689', description: 'Delicious Indian food at great prices!')
+
+
