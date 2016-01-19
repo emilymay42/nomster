c387251c58c178a9dc24b557c405dfa953e4c86e
Emily Nelson <emilymay42@gmail.com>
2016-01-19 18:38:05 +0000

    Added db and connections for photos

---
+++ db/migrate/20160119180000_create_photos.rb
@@ -0,0 +1,19 @@
+class CreatePhotos < ActiveRecord::Migration
+  def change
+    create_table :photos do |t|
+    	t.text :caption
+
+    	t.string :content_type
+    	t.string :filename
+    	t.binary :image_data
+
+    	t.integer :user_id
+    	t.integer :place_id
+
+      t.timestamps
+    end
+
+    add_index :photos, [:user_id, :place_id]
+    add_index :photos, :place_id
+  end
+end
