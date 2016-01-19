8b139f3d11c07acbbe8fea7a7b7afd3d1f648e9e
Emily Nelson <emilymay42@gmail.com>
2016-01-19 19:17:24 +0000

    Display captions on place page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,7 +3,8 @@ class PhotosController < ApplicationController
 
 	def create
 		@place = Place.find(params[:place_id])
-		
+		@place.photos.create(photo_params.merge(user: current_user))
+		redirect_to place_path(@place)
 
 	end
 
@@ -11,4 +12,5 @@ class PhotosController < ApplicationController
 
 	def photo_params
 		params.require(:photo).permit(:caption)
+	end
 end
