bcea581d5ccf8c81db109b53a994738544685e6e
Emily Nelson <emilymay42@gmail.com>
2016-01-22 04:03:16 +0000

    Modified email depending on Place

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,11 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
-  	mail(to: "emilymay42@gmail.com", subject: "A comment had been added to your place")
-  end
+  def comment_added (comment)
+	@place = comment.place
+  	@place_owner = @place.user
 
+  	mail(to: @place_owner.email, 
+  		subject: "A comment had been added to #{@place.name}")
+  end
 end
